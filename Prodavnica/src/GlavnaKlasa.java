22c54d81cd7f0a7a50fb06ccca2d7e68d5f223e4
milos.romcevic <milos.romcevic@HP-ROMCEVIC.gazela.local>
2019-04-15 13:18:01 +0200

    dodao glavnu klasu

--- Prodavnica/src/GlavnaKlasa.java
+++ Prodavnica/src/GlavnaKlasa.java
@@ -2,7 +2,7 @@
 public class GlavnaKlasa {
 
 	public static void main(String[] args) {
-		System.out.println("Ovo je glavna klasa, videcemo sta cemo dalje");
+		System.out.println("Evo je glavna klasa");
 
 	}
 
