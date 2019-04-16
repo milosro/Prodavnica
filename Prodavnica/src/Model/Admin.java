c32c6e0bb3b275845191db7edbaafbdd5fcfe86d
milos.romcevic <milos.romcevic@HP-ROMCEVIC.gazela.local>
2019-04-16 09:36:25 +0200

    Nove klase Admin, Product i User i Hibernate.cfg.xml fajl.

---
+++ Prodavnica/src/Model/Admin.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Admin {
+	
+	final String adminUserName = "admin";
+	final String password = "admin";
+	
+	public String getAdminUserName() {
+		return adminUserName;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+}
---
+++ Prodavnica/src/Model/User.java
@@ -0,0 +1,55 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+	
+	private int idUser;
+	private String userName;
+	private String password;
+	private double balance;
+	
+	List<Product> listOfProducts = new ArrayList<Product>();
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public List<Product> getListOfProducts() {
+		return listOfProducts;
+	}
+
+	public void setListOfProducts(List<Product> listOfProducts) {
+		this.listOfProducts = listOfProducts;
+	}
+
+}
