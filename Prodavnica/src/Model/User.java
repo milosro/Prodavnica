105a4619fe044ad36df09b5b017594b5e14849a7
milos.romcevic <milos.romcevic@HP-ROMCEVIC.gazela.local>
2019-04-16 09:50:13 +0200

    Dodao sa entitete

--- Prodavnica/src/Model/User.java
+++ Prodavnica/src/Model/User.java
@@ -3,8 +3,16 @@ package Model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int idUser;
 	private String userName;
 	private String password;
